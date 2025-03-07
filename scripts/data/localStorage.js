b658cd7347aca54bad489b982a3dcb5a93fafde2
StefanMogren <stefan_mogren@hotmail.com>
2025-03-07 14:58:14 +0100

    Added ;

--- scripts/data/localStorage.js
+++ scripts/data/localStorage.js
@@ -4,12 +4,12 @@ function getDataFromLocalStorage(keyname) {
     const getData = localStorage.getItem(keyname) || '[]';
     // Strängarrayen omvandlas till en array genom JSON.parse och returneras antingen tom eller med innehåll    
     return JSON.parse(getData);
-}
+};
 
 // Funktion för att spara i localStorage där det behöver skickas med en array samt keyname som måste innehålla '', t ex 'users'
 function saveDataToLocalStorage(keyname, array) {
     // Här skrivs det över den befintliga t ex 'users' i localStorage.
     localStorage.setItem(keyname, JSON.stringify(array));
-}
+};
 
 export {getDataFromLocalStorage, saveDataToLocalStorage};
