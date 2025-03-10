8f549c835fe3a8207029aadf5912c633f411b864
StefanMogren <stefan_mogren@hotmail.com>
2025-03-10 16:08:55 +0100

    Added so each product item can be displayed on screen

--- scripts/pages/menuPage.js
+++ scripts/pages/menuPage.js
@@ -1,5 +1,39 @@
-function runMenuPage() {
-	console.log('I am running the menu page!');
+import { fetchProducts } from '../api/api.js';
+async function runMenuPage() {
+	const products = await fetchProducts();
+	const cardContainerRef = document.querySelector('#cardContainer');
+
+	cardContainerRef.appendChild(createCards(products, true));
+}
+
+function createCards(products, addSecondText, addButton) {
+	console.log(products.items);
+
+	const unorderedListHTML = document.createElement('ul');
+
+	products.items.forEach((item) => {
+		const listItemHTML = document.createElement('li');
+		const sectionHTML = document.createElement('section');
+
+		const mainParagraphHTML = document.createElement('p');
+		mainParagraphHTML.innerHTML = `<span>${item.name}</span><span>${item.price} sek</span>`;
+		sectionHTML.appendChild(mainParagraphHTML);
+
+		if (addSecondText === true) {
+			const contentParagraphHTML = document.createElement('p');
+			contentParagraphHTML.textContent = item.description;
+			sectionHTML.appendChild(contentParagraphHTML);
+		}
+
+		const buttonHTML = document.createElement('button');
+		buttonHTML.textContent = '+';
+
+		listItemHTML.appendChild(sectionHTML);
+		listItemHTML.appendChild(buttonHTML);
+		unorderedListHTML.appendChild(listItemHTML);
+	});
+
+	return unorderedListHTML;
 }
 
 export { runMenuPage };
