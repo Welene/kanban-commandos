93ac94e26737ec808c1c6bfb6a4fae8be45d6d18
DavidAkerlind <david.akerlind@icloud.com>
2025-03-10 10:16:10 +0100

    Refactor script.js for consistent quote usage and simplify path logging

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,37 @@
-import { fetchProducts, fetchUsers } from "./api/api.js";
+import { fetchProducts, fetchUsers } from './api/api.js';
 import {
     getDataFromLocalStorage,
     saveDataToLocalStorage,
-} from "./data/localStorage.js";
-import { highlightActiveBurgerLink } from "./utils/utils.js";
-import { renderHamburgerMenu } from "./components/navMenu.js";
+} from './data/localStorage.js';
+import { highlightActiveBurgerLink } from './utils/utils.js';
+import { renderHamburgerMenu } from './components/navMenu.js';
 
-console.log("Script.js loaded");
+console.log('Script.js loaded');
 
 const path = window.location.pathname;
 
-if (path === "/pages/" || path === "/pages/index.html") {
-    console.log("index.html");
-} else if (path === "/pages/about.html") {
-    console.log("about.html");
-} else if (path === "/pages/eta.html") {
-    console.log("eta.html");
-} else if (path === "/pages/history.html") {
-    console.log("history.html");
-} else if (path === "/pages/map.html") {
-    console.log("map.html");
-} else if (path === "/pages/menu.html") {
-    console.log("menu.html");
-} else if (path === "/pages/order-overview.html") {
-    console.log("order-overview.html");
-} else if (path === "/pages/orders.html") {
-    console.log("orders.html");
-} else if (path === "/pages/profile.html") {
-    console.log("profile.html");
-} else if (path === "/pages/receipt.html") {
-    console.log("receipt.html");
-} else if (path === "/pages/register.html") {
-    console.log("register.html");
+if (path === '/pages/' || path === '/pages/index.html') {
+    console.log('index.html');
+} else if (path === '/pages/about.html') {
+    console.log('about.html');
+} else if (path === '/pages/eta.html') {
+    console.log('eta.html');
+} else if (path === '/pages/history.html') {
+    console.log('history.html');
+} else if (path === '/pages/map.html') {
+    console.log('map.html');
+} else if (path === '/pages/menu.html') {
+    console.log('menu.html');
+} else if (path === '/pages/order-overview.html') {
+    console.log('order-overview.html');
+} else if (path === '/pages/orders.html') {
+    console.log('orders.html');
+} else if (path === '/pages/profile.html') {
+    console.log('profile.html');
+} else if (path === '/pages/receipt.html') {
+    console.log('receipt.html');
+} else if (path === '/pages/register.html') {
+    console.log('register.html');
 }
 
 renderHamburgerMenu();
