460e221d752a1e641ae6f8ecb4973ec62f1ff784
lammai442 <lam.mai442@gmail.com>
2025-03-10 14:13:02 +0100

    Created setup pages for javaScript and added call function from all pages to script.js

---
+++ scripts/pages/receiptPage.js
@@ -0,0 +1,5 @@
+function runReceiptPage() {
+
+}
+
+export { runReceiptPage }
--- scripts/script.js
+++ scripts/script.js
@@ -5,37 +5,59 @@ import {
 } from './data/localStorage.js';
 import { highlightActiveBurgerLink, clickLoginBtn } from './utils/utils.js';
 import { renderHamburgerMenu, openNavMenu } from './components/navMenu.js';
+import { runIndexPage } from './pages/indexPage.js'
+import { runAboutPage } from './pages/aboutPage.js';
+import { runEtaPage } from './pages/etaPage.js';
+import { runHistoryPage } from './pages/historyPage.js';
+import { runMapPage } from './pages/mapPage.js';
+import { runMenuPage } from './pages/menuPage.js';
+import { runOrderOverviewPage } from './pages/order-overviewPage.js';
+import { runOrdersPage } from './pages/ordersPage.js';
+import { runProfilePage } from './pages/profilePage.js';
+import { runReceiptPage } from './pages/receiptPage.js';
+import { runRegisterPage } from './pages/registerPage.js';
 
 console.log('Script.js loaded');
 
 const path = window.location.pathname;
 
 if (path === '/pages/' || path === '/pages/index.html') {
-    openNavMenu();
+    runIndexPage();
     console.log('index.html');
 } else if (path === '/pages/about.html') {
+    runAboutPage();
     console.log('about.html');
 } else if (path === '/pages/eta.html') {
+    runEtaPage();
     console.log('eta.html');
 } else if (path === '/pages/history.html') {
+    runHistoryPage();
     console.log('history.html');
 } else if (path === '/pages/map.html') {
+    runMapPage();
     console.log('map.html');
 } else if (path === '/pages/menu.html') {
+    runMenuPage();
     console.log('menu.html');
 } else if (path === '/pages/order-overview.html') {
+    runOrderOverviewPage();
     console.log('order-overview.html');
 } else if (path === '/pages/orders.html') {
+    runOrdersPage();
     console.log('orders.html');
 } else if (path === '/pages/profile.html') {
+    runProfilePage();
     console.log('profile.html');
 } else if (path === '/pages/receipt.html') {
+    runReceiptPage();
     console.log('receipt.html');
 } else if (path === '/pages/register.html') {
+    runRegisterPage();
     console.log('register.html');
 }
 
+openNavMenu();
 renderHamburgerMenu();
 highlightActiveBurgerLink();
-// calls function that navigates you to menu.html when clicking on "Logga inn" in index page
-clickLoginBtn();
+
+
