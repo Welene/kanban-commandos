9a18a03e3f3cceee6a1a4b765c79b554e5cc9a87
Welene <helenetheo@live.no>
2025-03-11 17:24:51 +0100

    Removed IF x2 from eta.js

--- scripts/pages/etaPage.js
+++ scripts/pages/etaPage.js
@@ -11,21 +11,16 @@ function clickEtaBtns () {
     let newOrderBtn = document.getElementById('newOrderBtn');
     let receiptBtn = document.getElementById('receiptBtn');
 
+    // Event listener on both buttons to navigate to the correct page
+    newOrderBtn.addEventListener('click', () => {
+        window.location.href = '../pages/menu.html';
+        console.log('Navigated to menu.html after clicking New Order button');
+    });
 
-    // event listener on both buttons to navigate to the correct page
-    if (newOrderBtn) {
-        newOrderBtn.addEventListener('click', () => {
-            window.location.href = '../pages/menu.html';
-            console.log('Navigated to menu.html after clicking New Order button');
-        });
-    }
-
-    if (receiptBtn) {
-        receiptBtn.addEventListener('click', () => {
-            window.location.href = '../pages/receipt.html';
-            console.log('Navigated to receipt.html after clicking Show Receipt button');
-        });
-    }
+    receiptBtn.addEventListener('click', () => {
+        window.location.href = '../pages/receipt.html';
+        console.log('Navigated to receipt.html after clicking Show Receipt button');
+    });
 }
 
 // TLDR: 
