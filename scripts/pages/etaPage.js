764d13bde8cc1c060195bc42c0d37004b605a131
Welene <helenetheo@live.no>
2025-03-11 15:23:02 +0100

    Made displayReceiptNumber // GOT UNDEFINED ALL THE TIME // forgot to call the function in runEtaPage... // WORKS NOW :-)

--- scripts/pages/etaPage.js
+++ scripts/pages/etaPage.js
@@ -1,6 +1,7 @@
 function runEtaPage() {
     clickEtaBtns()
     displayEtaText()
+    displayReceiptNumber()
 }
 
 export { runEtaPage };
@@ -26,12 +27,14 @@ function clickEtaBtns () {
     }
 }
 
-// TLDR: getting basket from localStorage -- setting 0 as start time -- if basket has more than 0 items = use reduce (reduces all items/item times to one combined Eta time) 
-// -- adds one item AKA 1 min to totalEta at a time, tells it to start at 0 -- returns totalEta that gets put into `${totalEta}` later. -- catches error if there are 
-// no food items = returns 0 (minutes)
+// TLDR: 
+// getting basket from localStorage -- setting 0 as start time -- if basket has more than 0 items = use reduce (reduces all items/item times to one combined Eta time)
+// if the basket has no items then totalEta is automatically 0 minutes.
+// -- adds one item AKA 1 min to totalEta at a time, tells it to start at 0 -- returns totalEta that gets put into `${totalEta}` later. 
+// -- catches error if there are no food items = returns 0 (minutes)
 function calculateEta() {
     try {
-        let basket = JSON.parse(localStorage.getItem("basket")) || { items: [] }; 
+        let basket = JSON.parse(localStorage.getItem('basket')) || { items: [] }; 
         let totalEta = 0; 
         if (basket.items.length > 0) { 
             totalEta += basket.items.reduce((sum, item) => sum + item.amount, 0); 
@@ -39,18 +42,31 @@ function calculateEta() {
         return totalEta;
 
     } catch (error) {
-        console.log("No orders found, 0 minutes", error);
+        console.log('No orders found, 0 minutes', error);
         return 0; 
     }
 }
 
 
-// TLDR: function that displays totalEta text using/running the calculateEta function, also adding ETA + MIN to the textContent as well as total minutes
+// TLDR: 
+// function that displays totalEta text using/running the calculateEta function, also adding ETA + MIN to the textContent as well as total minutes in between those words
 function displayEtaText() {
     let totalEta = calculateEta();
-    document.getElementById("etaText").textContent = `ETA ${totalEta} MIN`;
+    document.getElementById('etaText').textContent = `ETA ${totalEta} MIN`;
 }
 
-// function display receiptNumber () {
-// document.getElementById("receiptNumber").textContent = `#${receiptNumber}`;
-// }
+// TLDR: 
+// function that displays receiptNumber for spesific order -- gets basket from localStorage -- shows empty objekt if there is nothing in the basket
+// gets the id from basket (the receipt number for the spesific order) and saves it in variable receiptNumber
+// if receiptNumber (basket.id) does not exist/or cannot find it then you will see an error message in the console
+// gets receiptNumber id from html to change textContent to # + the number that will be made later in a function and stored in localStorage
+function displayReceiptNumber () {
+    let basket = JSON.parse(localStorage.getItem('basket')) || {};
+    let receiptNumber = basket.id
+
+    if (!receiptNumber) {
+        console.log('Cannot find any basket id');
+    } else {
+        document.getElementById('receiptNumber').textContent = `#${receiptNumber}`;
+    }
+}
