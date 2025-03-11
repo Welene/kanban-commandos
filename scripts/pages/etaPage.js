248c79f9ecb7a1a4b5b564041a6f3ae4aea019f9
Welene <helenetheo@live.no>
2025-03-11 14:34:23 +0100

    added function that calculates ETA from amount of food items added in localStorage. // started on display etaText & receiptNumber functions

--- scripts/pages/etaPage.js
+++ scripts/pages/etaPage.js
@@ -24,3 +24,31 @@ function clickEtaBtns () {
         });
     }
 }
+
+// TLDR: getting basket from localStorage -- setting 0 as start time -- if basket has more than 0 items = use reduce (reduces all items/item times to one combined Eta time) 
+// -- adds one item AKA 1 min to totalEta at a time, tells it to start at 0 -- returns totalEta which we can put into `${etaMinutes}` later. -- catches error if there are 
+// no food items = returns 0 (minutes)
+function calculateEta() {
+    try {
+        let basket = JSON.parse(localStorage.getItem("basket")); 
+        let totalEta = 0; 
+        if (basket.items.length > 0) { 
+            totalEta += basket.items.reduce((sum, item) => sum + item.amount, 0); 
+        }
+        return totalEta;
+
+    } catch (error) {
+        console.log("No orders found, 0 minutes", error);
+        return 0; 
+    }
+}
+
+
+
+function display etaText () {
+    document.getElementById("etaText").textContent = `ETA ${etaMinutes} MIN`;
+}
+
+function display receiptNumber () {
+document.getElementById("receiptNumber").textContent = `#${receiptNumber}`;
+}
