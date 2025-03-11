c2e7c5a0fddd8ed5636edd57599b5a3e7c48a0cc
DavidAkerlind <david.akerlind@icloud.com>
2025-03-11 09:45:50 +0100

    Started on add to basket function

--- scripts/components/addToBasket.js
+++ scripts/components/addToBasket.js
@@ -1,1 +1,17 @@
 // Här ska funktionen för att lägga till produkter i kundvagnen
+
+import {
+    getDataFromLocalStorage,
+    saveDataToLocalStorage,
+} from '../data/localStorage.js';
+
+function getBasketItems() {
+    let basketData = getDataFromLocalStorage('basket');
+    return basketData;
+}
+
+export function addToBasket(id, itemName, price) {
+    let itemId = elem.getAttribute('data-id');
+    console.log(itemId);
+    let itemToAdd = {};
+}
