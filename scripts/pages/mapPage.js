cdb1d7a247944eca121a23293b86a8b0493d2ee3
StefanMogren <stefan_mogren@hotmail.com>
2025-03-12 15:49:20 +0100

    Changed map images, finished changeMap function, added dynamic marker on the map, added foodtruck name

--- scripts/pages/mapPage.js
+++ scripts/pages/mapPage.js
@@ -9,6 +9,10 @@ function runMapPage() {
 function changeMap() {
 	const selectionFoodtruckRef = document.querySelector('#selectionFoodtruck');
 	selectionFoodtruckRef.addEventListener('change', (selection) => {
+		// Gör så containern med namnet, kartan och markören blir synlig
+		const imgContainer = document.querySelector('#imgContainer');
+		imgContainer.classList.remove('d-none');
+
 		// Bilden ändras beroende på vad man väljer i drop-down menyn.
 		const mapImgRef = document.querySelector('#mapImg');
 		mapImgRef.src = `../assets/maps/${selection.target.value}.jpg`;
@@ -19,6 +23,28 @@ function changeMap() {
 
 		// Ändrar alt-texten beroende på valet
 		mapImgRef.alt = `Karta med footruck vid ${selectionText}`;
+
+		const foodTruckName = document.querySelector('#foodTruckName');
+		const mapMarkerRef = document.querySelector('#mapMarker');
+
+		// Placeholderfunktionalitet för att visa vart på kartan som foodtrucken finns.
+		// Även visa namnet på foodtrucken
+		if (selection.target.selectedIndex === 1) {
+			foodTruckName.textContent = 'Bengts Wontons';
+			mapMarkerRef.textContent = 'B';
+			mapMarkerRef.style.top = '36%';
+			mapMarkerRef.style.left = '43%';
+		} else if (selection.target.selectedIndex === 2) {
+			foodTruckName.textContent = 'Foodtruckexperten';
+			mapMarkerRef.textContent = 'F';
+			mapMarkerRef.style.top = '49%';
+			mapMarkerRef.style.left = '30%';
+		} else if (selection.target.selectedIndex === 3) {
+			foodTruckName.textContent = 'Super Wonton Meals';
+			mapMarkerRef.textContent = 'S';
+			mapMarkerRef.style.top = '53%';
+			mapMarkerRef.style.left = '43%';
+		}
 	});
 }
 
