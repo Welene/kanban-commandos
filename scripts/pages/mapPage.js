67b1064d0033d124b139a6f4528ebf6d3a155f0a
StefanMogren <stefan_mogren@hotmail.com>
2025-03-12 13:48:17 +0100

    Changed name of one map image, added full functionality to mapPage.js

--- scripts/pages/mapPage.js
+++ scripts/pages/mapPage.js
@@ -1,8 +1,25 @@
-import { doesBasketItemCountsExist } from "../utils/utils.js";
+import { doesBasketItemCountsExist } from '../utils/utils.js';
 
 function runMapPage() {
-    // Funktion för att skapa röda cirkeln runt basket om det finns tillagda items
-    doesBasketItemCountsExist()
+	// Funktion för att skapa röda cirkeln runt basket om det finns tillagda items
+	doesBasketItemCountsExist();
+	changeMap();
 }
 
-export { runMapPage }
+function changeMap() {
+	const selectionFoodtruckRef = document.querySelector('#selectionFoodtruck');
+	selectionFoodtruckRef.addEventListener('change', (selection) => {
+		// Bilden ändras beroende på vad man väljer i drop-down menyn.
+		const mapImgRef = document.querySelector('#mapImg');
+		mapImgRef.src = `../assets/maps/${selection.target.value}.jpg`;
+
+		// Letar upp textinnehållet i det man väljer
+		const selectionText =
+			selection.target.options[selection.target.selectedIndex].text;
+
+		// Ändrar alt-texten beroende på valet
+		mapImgRef.alt = `Karta med footruck vid ${selectionText}`;
+	});
+}
+
+export { runMapPage };
