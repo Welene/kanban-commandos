b4fec0410dfab1682dd267dd69b76ff9cb7be5fb
Robin-Torp <3kdyhfe24.torpro@folkuniversitetet.nu>
2025-03-12 13:57:23 +0100

    Klar med receipt page,CSS samt JS

--- scripts/pages/receiptPage.js
+++ scripts/pages/receiptPage.js
@@ -1,5 +1,9 @@
-function runReceiptPage() {
+import { generateReceipt } from '../components/createReceipt.js';
+import { doesBasketItemCountsExist } from '../utils/utils.js';
 
+function runReceiptPage() {
+    generateReceipt();
+    doesBasketItemCountsExist();
 }
 
-export { runReceiptPage }
+export { runReceiptPage };
