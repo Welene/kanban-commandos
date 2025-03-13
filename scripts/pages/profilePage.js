482f94fb9c9bc9903584b7134fa050de44b39f4d
lammai442 <lam.mai442@gmail.com>
2025-03-13 10:10:05 +0100

    Started on profile edit page with HTML and functions

--- scripts/pages/profilePage.js
+++ scripts/pages/profilePage.js
@@ -1,8 +1,15 @@
-import { doesBasketItemCountsExist } from "../utils/utils.js";
+import { doesBasketItemCountsExist } from '../utils/utils.js';
+import { getDataFromLocalStorage, saveDataToLocalStorage } from '../data/localStorage.js'
     
 function runProfilePage() {
     // Funktion för att skapa röda cirkeln runt basket om det finns tillagda items
     doesBasketItemCountsExist()
 }
+
+function getUserInfo {
+
+}
+
+
 
 export { runProfilePage }
