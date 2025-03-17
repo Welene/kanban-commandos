d46cf5ef3690a93205cc0e0d0c38a6c2c26fc0f1
StefanMogren <stefan_mogren@hotmail.com>
2025-03-17 11:42:04 +0100

    Added base content to order-overview.html, created order-overview-css, added base css, added initial js to order-overviewPage.js, removed basket call from order-overview in script.js

--- scripts/pages/order-overviewPage.js
+++ scripts/pages/order-overviewPage.js
@@ -1,5 +1,127 @@
-function runOrderOverviewPage() {
+import {
+	getDataFromLocalStorage,
+	saveDataToLocalStorage,
+} from '../data/localStorage.js';
 
+function runOrderOverviewPage() {
+	const myData = getDataFromLocalStorage('users');
+
+	myData.forEach((user) => {
+		console.log(user.username);
+		console.log(user.receipts);
+		user.receipts.forEach((receipt) => {
+			console.log(receipt.foodTruck);
+		});
+		console.log('----');
+	});
 }
 
-export { runOrderOverviewPage }
+/* 
+* Behöver localStorage som sparar alla kvitton
+
+* Behöver localStorage som även sparar foodtruck
+
+users -> varje users -> receipts -> spara varje receipt -> kolla vilken foodTruck om användes
+
+*/
+
+/* const newUsers = [
+		{
+			username: 'jesperdaking',
+			password: 'fisnils123',
+			role: 'admin',
+			email: 'jesper123@airbean.com',
+			profile_image: 'https://randomuser.me/api/portraits/men/1.jpg',
+			receipts: [
+				{
+					id: '#rz07pq8',
+					foodTruck: 'truck2',
+					items: [{ id: 2, name: 'Bangkok', amount: 1, price: 9 }],
+				},
+			],
+		},
+
+		{
+			username: 'bradpitt',
+			password: 'fightclub9',
+			role: 'user',
+			email: 'bradpitt@coffeeclique.com',
+			profile_image: 'https://randomuser.me/api/portraits/men/2.jpg',
+			receipts: [
+				{
+					id: '#7z2gz58',
+					foodTruck: 'truck1',
+					items: [{ id: 1, name: 'Karlstad', amount: 1, price: 9 }],
+				},
+				{
+					id: '#g42jbr',
+					foodTruck: 'truck3',
+					items: [
+						{ id: 2, name: 'Bangkok', amount: 2, price: 9 },
+						{ id: 1, name: 'Karlstad', amount: 1, price: 9 },
+						{ id: 3, name: 'Ho Chi Minh', amount: 1, price: 9 },
+					],
+				},
+			],
+		},
+		{
+			username: 'fotbollskungen',
+			password: 'legomast3r',
+			role: 'user',
+			email: 'fotbollskungen@brewbuddies.com',
+			profile_image: 'https://randomuser.me/api/portraits/men/3.jpg',
+			receipts: [
+				{
+					id: '#7z2gz58',
+					foodTruck: 'truck1',
+					items: [{ id: 1, name: 'Karlstad', amount: 1, price: 9 }],
+				},
+				{
+					id: '#2iu9hjq',
+					foodTruck: 'truck3',
+					items: [
+						{ id: 1, name: 'Karlstad', amount: 1, price: 9 },
+						{ id: 2, name: 'Bangkok', amount: 1, price: 9 },
+						{ id: 3, name: 'Ho Chi Minh', amount: 1, price: 9 },
+					],
+				},
+				{
+					id: '#9976l2r',
+					foodTruck: 'truck3',
+					items: [
+						{ id: 2, name: 'Bangkok', amount: 2, price: 9 },
+						{ id: 1, name: 'Karlstad', amount: 1, price: 9 },
+						{ id: 3, name: 'Ho Chi Minh', amount: 1, price: 9 },
+					],
+				},
+				{
+					id: '#w0x3a6g',
+					foodTruck: 'truck2',
+					items: [
+						{ id: 2, name: 'Bangkok', amount: 4, price: 9 },
+						{ id: 1, name: 'Karlstad', amount: 5, price: 9 },
+						{ id: 3, name: 'Ho Chi Minh', amount: 3, price: 9 },
+						{ id: 5, name: 'Oaxaca', amount: 4, price: 9 },
+						{ id: 4, name: 'Paris', amount: 2, price: 9 },
+						{ id: 12, name: 'Sprite', amount: 1, price: 19 },
+					],
+				},
+				{
+					id: '#ke8qxvc',
+					foodTruck: 'truck2',
+					items: [{ id: 1, name: 'Karlstad', amount: 1, price: 9 }],
+				},
+				{
+					id: '#isefu7q',
+					foodTruck: 'truck3',
+					items: [
+						{ id: 1, name: 'Karlstad', amount: 1, price: 9 },
+						{ id: 4, name: 'Paris', amount: 1, price: 9 },
+						{ id: 8, name: 'Guacamole', amount: 1, price: 19 },
+					],
+				},
+			],
+		},
+]; */
+
+export { runOrderOverviewPage };
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 import { fetchProducts, fetchUsers } from './api/api.js';
 import {
-    getDataFromLocalStorage,
-    saveDataToLocalStorage,
+	getDataFromLocalStorage,
+	saveDataToLocalStorage,
 } from './data/localStorage.js';
 import { renderHamburgerMenu } from './components/navMenu.js';
 import { runIndexPage } from './pages/indexPage.js';
@@ -22,56 +22,55 @@ console.log('Script.js loaded');
 const path = window.location.pathname;
 
 if (path === '/pages/' || path === '/pages/index.html') {
-    runIndexPage();
-    console.log('index.html');
+	runIndexPage();
+	console.log('index.html');
 } else if (path === '/pages/about.html') {
-    runAboutPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('about.html');
+	runAboutPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('about.html');
 } else if (path === '/pages/eta.html') {
-    runEtaPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('eta.html');
+	runEtaPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('eta.html');
 } else if (path === '/pages/history.html') {
-    runHistoryPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('history.html');
+	runHistoryPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('history.html');
 } else if (path === '/pages/map.html') {
-    runMapPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('map.html');
+	runMapPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('map.html');
 } else if (path === '/pages/menu.html') {
-    runMenuPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('menu.html');
+	runMenuPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('menu.html');
 } else if (path === '/pages/order-overview.html') {
-    runOrderOverviewPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('order-overview.html');
+	runOrderOverviewPage();
+	renderHamburgerMenu();
+	console.log('order-overview.html');
 } else if (path === '/pages/orders.html') {
-    runOrdersPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('orders.html');
+	runOrdersPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('orders.html');
 } else if (path === '/pages/profile.html') {
-    runProfilePage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('profile.html');
+	runProfilePage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('profile.html');
 } else if (path === '/pages/receipt.html') {
-    runReceiptPage();
-    renderHamburgerMenu();
-    openDropDownBasket();
-    console.log('receipt.html');
+	runReceiptPage();
+	renderHamburgerMenu();
+	openDropDownBasket();
+	console.log('receipt.html');
 } else if (path === '/pages/register.html') {
-    runRegisterPage();
-    console.log('register.html');
+	runRegisterPage();
+	console.log('register.html');
 }
 
 // calls function that navigates you to menu.html when clicking on "Logga inn" in index page
